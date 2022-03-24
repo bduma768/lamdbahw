7e7f71d8e051b18832e67cdee2d1f11895171150
bduma768 <102253471+bduma768@users.noreply.github.com>
2022-03-24 02:57:08 +0000

    Add files via upload

---
+++ Function.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+
+namespace lambda_hw
+{
+    public class Function
+    {
+        public string FunctionHandler(string input, ILambdaContext context)
+        {
+            return input.ToUpper();
+        }
+    }
+}
+
+
+//lambda_hw::lambda_hw.Function::FunctionHandler
